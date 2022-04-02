122fe7442587d043f927e4b07077ba578af4fc0d
Carlos André de Almeida Cavalcante <carlos.andre1270@gmail.com>
2022-04-02 12:19:25 -0300

    adding registry unit tests

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -15,9 +15,7 @@ class RegistrationTest extends TestCase
 
     public function test_registration_screen_can_be_rendered()
     {
-        if (! Features::enabled(Features::registration())) {
-            return $this->markTestSkipped('Registration support is not enabled.');
-        }
+        $this->check_registration();
 
         $response = $this->get('/register');
 
@@ -37,9 +35,7 @@ class RegistrationTest extends TestCase
 
     public function test_new_users_can_register()
     {
-        if (! Features::enabled(Features::registration())) {
-            return $this->markTestSkipped('Registration support is not enabled.');
-        }
+        $this->check_registration();
 
         $response = $this->post('/register', [
             'profile' => User::PROFILE_ENUM['entrepreneur'],
@@ -55,5 +51,95 @@ class RegistrationTest extends TestCase
 
         $this->assertAuthenticated();
         $response->assertRedirect(RouteServiceProvider::HOME);
+    }
+
+    public function test_new_user_filling_in_wrong_cpf() {
+        $this->check_registration();
+
+        $response = $this->get('/register');
+
+        $response = $this->post('/register', [
+            'profile' => User::PROFILE_ENUM['entrepreneur'],
+            'nome' => 'Test User',
+            'email' => 'test@example.com',
+            'cpf' => '512.716.580-00', // Invalid CPF
+            'password' => 'password',
+            'password_confirmation' => 'password',
+            'data_de_nascimento' => date('1999-02-13'),
+            'sexo' => User::SEXO_ENUM['masculine'],
+            'termos' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
+        ]);
+
+        $this->assertGuest();
+        $response->assertRedirect(env('APP_URL') . '/register');
+    }
+
+    public function test_new_user_with_different_passwords() {
+        $this->check_registration();
+
+        $response = $this->get('/register');
+
+        $response = $this->post('/register', [
+            'profile' => User::PROFILE_ENUM['entrepreneur'],
+            'nome' => 'Test User',
+            'email' => 'test@example.com',
+            'cpf' => '512.716.580-00', // Invalid CPF
+            'password' => 'passwords',
+            'password_confirmation' => 'password',
+            'data_de_nascimento' => date('1999-02-13'),
+            'sexo' => User::SEXO_ENUM['masculine'],
+            'termos' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
+        ]);
+
+        $this->assertGuest();
+        $response->assertRedirect(env('APP_URL') . '/register');
+    }
+
+    public function test_new_user_not_filling_all_fields() {
+        $this->check_registration();
+
+        $response = $this->get('/register');
+
+        $response = $this->post('/register', [
+            'profile' => User::PROFILE_ENUM['entrepreneur'],
+            'nome' => 'Test User',
+            'email' => 'test@example.com',
+            'cpf' => '512.716.580-00', // Invalid CPF
+            'password' => 'password',
+            'password_confirmation' => 'password',
+            'data_de_nascimento' => null,
+            'sexo' => null,
+            'termos' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
+        ]);
+
+        $this->assertGuest();
+        $response->assertRedirect(env('APP_URL') . '/register');
+    }
+
+    public function new_user_without_choosing_your_profile() {
+        $this->check_registration();
+
+        $response = $this->get('/register');
+
+        $response = $this->post('/register', [
+            'profile' => null,
+            'nome' => 'Test User',
+            'email' => 'test@example.com',
+            'cpf' => '512.716.580-00', // Invalid CPF
+            'password' => 'password',
+            'password_confirmation' => 'password',
+            'data_de_nascimento' => date('1999-02-13'),
+            'sexo' => User::SEXO_ENUM['masculine'],
+            'termos' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
+        ]);
+
+        $this->assertGuest();
+        $response->assertRedirect(env('APP_URL') . '/register');
+    }
+
+    private function check_registration() {
+        if (! Features::enabled(Features::registration())) {
+            return $this->markTestSkipped('Registration support is not enabled.');
+        }
     }
 }
