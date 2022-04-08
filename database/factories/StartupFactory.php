220ef383efc24180a53d252a534965e014d56614
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2022-04-08 12:37:54 -0300

    adiciona factory da area e startup

--- database/factories/StartupFactory.php
+++ database/factories/StartupFactory.php
@@ -14,7 +14,11 @@ class StartupFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'nome' => $this->faker->company,
+            'descricao' => $this->faker->realText($maxNbChars = 200),
+            'email' => $this->faker->email,
+            'cnpj' => $this->faker->cnpj(false),
+            'logo' => 'startups/logos/'.$this->faker->image($dir = storage_path('app/public/startups/logos'), $width = 640, $height = 480, null, false),
         ];
     }
 }
