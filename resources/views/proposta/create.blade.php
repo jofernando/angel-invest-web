c9dab38c63aff830e2d364197bf40691e3cf0b1d
Carlos André de Almeida Cavalcante <carlos.andre1270@gmail.com>
2022-04-14 17:47:00 -0300

    ajustes em create e edit

--- resources/views/proposta/create.blade.php
+++ resources/views/proposta/create.blade.php
@@ -6,11 +6,11 @@
                     <div class="col-md-4 div-checks">
                         <div class="row mb-4 mt-4" style="text-align: center;">
                             <div class="col-md-12">
-                                <a href="{{route('propostas.index', $startup)}}" class="btn btn-success btn-padding border" style="font-size: 22px;"><img src="{{asset('img/back.svg')}}" alt="Icone de voltar" style="padding-right: 10px;"> Voltar</a>
+                                <a href="{{route('propostas.index', $startup)}}" class="btn btn-success btn-padding border"><img src="{{asset('img/back.svg')}}" alt="Icone de voltar" style="padding-right: 5px; height: 22px;"> Voltar</a>
                             </div>  
                         </div>
                         <div class="row">
-                            <h4 class="card-title">Adicionando nova proposta para {{$startup->nome}}</h5>
+                            <h4 class="card-title" style="font-size: 22px;">Adicionando nova proposta para {{$startup->nome}}</h5>
                         </div>
                     </div>
                     <div class="col-md-8 div-form">
@@ -76,16 +76,17 @@
                                                         <small>Tamanho máximo: <span style="color: red;">100 MB</span></small>
                                                     </div>
                                                 </div>
-
-                                                @error('vídeo_do_pitch')
-                                                    <div id="validationServer03Feedback" class="invalid-feedback">
+                                                <br>
+                                                <div id="pitch-mesage" class="alert alert-danger" role="alert" style="display: @error('vídeo_do_pitch') block @else none @enderror">
+                                                    @error('vídeo_do_pitch')
                                                         {{ $message }}
-                                                    </div>
-                                                @enderror
+                                                    @enderror
+                                                </div>
                                             </div>
                                             <div style="display: none;">
-                                                <input id="video_input" name="vídeo_do_pitch" type="file" class="form-control @error('vídeo_do_pitch') is-invalid @enderror" accept="video/mp4,video/mkv">
+                                                <input id="video_input" name="vídeo_do_pitch" type="file" class="form-control @error('vídeo_do_pitch') is-invalid @enderror" accept=".mp4, .mkv">
                                             </div>
+                                            
                                         </div>
                                     </div>
                                     <div class="col-md-6">
@@ -99,27 +100,28 @@
                                                 <div class="card card-click">
                                                     <div class="card-body">
                                                         <div style="text-align: center;">
-                                                            <img id="input-profile-photo-retangular" src="{{asset('img/img.svg')}}" alt="Imagem default perfil" style="width: 100%; height: 100px; max-width: 100%; max-height: 100px;">
+                                                            <img id="input-profile-photo-retangular" src="{{asset('img/img.svg')}}" alt="Imagem default perfil" style="width: 100%; height: 80px; max-width: 100%; max-height: 80px;">
                                                         </div>
                                                         <div>Faça o upload da thumbnail</div>
                                                         <small>Tamanho máximo: <span style="color: red;">5 MB</span></small>
                                                     </div>
                                                 </div>
-                                                @error('thumbnail')
-                                                    <div id="validationServer03Feedback" class="invalid-feedback">
+                                                <br>
+                                                <div id="thumbnail-mesage" class="alert alert-danger" role="alert" style="display: @error('thumbnail') block @else none @enderror">
+                                                    @error('thumbnail')   
                                                         {{ $message }}
-                                                    </div>
-                                                @enderror
+                                                    @enderror
+                                                </div>
                                             </div>
                                             <div style="display: none;">
-                                                <input id="thumbnail" name="thumbnail" type="file" class="form-control @error('thumbnail') is-invalid @enderror" accept="image/png,image/jpg">
+                                                <input id="thumbnail" name="thumbnail" type="file" class="form-control @error('thumbnail') is-invalid @enderror" accept=".png, .jpg">
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="row mt-4" style="text-align: center;">
                                     <div class="col-md-12">
-                                        <button class="btn btn-success" style="width: 40%;">Salvar</button>
+                                        <button class="btn btn-success submit-form-btn" style="width: 40%;">Salvar</button>
                                     </div>
                                 </div>
                                 
@@ -150,28 +152,44 @@
         function read_video() {
             if (this.files && this.files[0]) {
                 var file = this.files[0];
-                var blobURL = URL.createObjectURL(file);
+                var ptich_mesage = document.getElementById('pitch-mesage');
                 
-                document.getElementById("div-video-preview").innerHTML = '<video id="video-preview" controls style="width: 100%; height: 100px; max-width: 100%; max-height: 100px;">'
+                if (file.size > 104857600) { 
+                    ptich_mesage.style.display = "block";
+                    ptich_mesage.innerHTML = "O campo thumbnail não pode ser superior a 1024000 kilobytes."
+                } else {
+                    var blobURL = URL.createObjectURL(file);
+                    ptich_mesage.style.display = "none";
+                    document.getElementById("div-video-preview").innerHTML = '<video id="video-preview" controls style="width: 100%; height: 100px; max-width: 100%; max-height: 100px;">'
                                             +'<source src="'+ blobURL +'">'
                                             +'Seu navegador não suporta vídeo HTML5.</video>';
                 
-                // document.querySelector("#video-preview").play();
 
-                div_img = document.getElementById('div-img-video-input');
-                div_video = document.getElementById('div-video-preview');
-                div_img.style.display = "none";
-                div_video.style.display = "block";
+                    div_img = document.getElementById('div-img-video-input');
+                    div_video = document.getElementById('div-video-preview');
+                    div_img.style.display = "none";
+                    div_video.style.display = "block";
+                }
             }
         }
 
         function read_image() {
             if (this.files && this.files[0]) {
-                var file = new FileReader();
-                file.onload = function(e) {
-                    document.getElementById("input-profile-photo-retangular").src = e.target.result;
-                };       
-                file.readAsDataURL(this.files[0]);
+                var ptich_mesage = document.getElementById('thumbnail-mesage');
+
+                if (this.files[0].size > 5242880) { 
+                    ptich_mesage.style.display = "block";
+                    ptich_mesage.innerHTML = "O campo thumbnail não pode ser superior a 5120 kilobytes."
+                } else {
+                    var file = new FileReader();
+                    ptich_mesage.style.display = "none";
+
+                    file.onload = function(e) {
+                        document.getElementById("input-profile-photo-retangular").src = e.target.result;
+                    };       
+                    file.readAsDataURL(this.files[0]);
+                }
+                
             }
         }
 
