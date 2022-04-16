c4004f742c24bbd3fc07a8ff3f5a1f21eeaaf142
Aline Costa <linecosta04@gmail.com>
2022-04-16 17:50:32 -0300

    criando testes de integração de endereço

---
+++ tests/Feature/Endereco/EnderecoTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature\Endereco;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+Use App\Models\Area;
+use App\Models\Startup;
+use App\Models\Endereco;
+
+class EnderecoTest extends TestCase
+{
+    protected function criar_startup()
+    {
+        $this->actingAs($user = User::factory()->create());
+        $area = Area::factory()->create();
+        return Startup::factory()->createStartup($user, $area);
+    }
+
+    protected function criar_endereco()
+    {
+        $this->actingAs($user = User::factory()->create());
+        $area = Area::factory()->create();
+        $startup = Startup::factory()->createStartup($user, $area);
+        return Endereco::factory()->createEndereco($startup);
+    }
+
+    protected function get_array_endereco($cep, $bairro, $rua, $numero,$estado, $cidade, $complemento)
+    {
+        return [
+            'cep' => $cep,
+            'bairro' => $bairro,
+            'rua' => $rua,
+            'numero' => $numero,
+            'estado' => $estado,
+            'cidade' => $cidade,
+            'complemento' => $complemento,
+        ];
+    }
+}
