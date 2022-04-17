76ef83fa906a1bd8660d8cea582e67739b45b248
Aline Costa <linecosta04@gmail.com>
2022-04-17 00:48:19 -0300

    Adicionando policy de endereço

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -16,6 +16,7 @@ class AuthServiceProvider extends ServiceProvider
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
         'App\Models\Startup' => 'App\Policies\StartupPolicy',
         'App\Models\Proposta' => 'App\Policies\PropostaPolicy',
+        'App\Models\Endereco' => 'App\Policies\EnderecoPolicy',
     ];
 
     /**
